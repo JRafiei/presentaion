a538c1576950f3e4b6eae51eab55f00b8fbbe2ec
MJR <mohammad.rafiei69@gmail.com>
2019-04-16 10:01:04 +0430

    add favicon and use sanic's static file handler

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from sanic import Sanic
 from sanic.response import json, html
 from sanic.websocket import WebSocketProtocol
@@ -5,6 +6,10 @@ from sanic.websocket import WebSocketProtocol
 app = Sanic()
 
 clients = set()
+
+STATIC_FOLDER = os.path.join(os.path.dirname(__file__), 'static')
+app.static('/favicon.ico', os.path.join(STATIC_FOLDER, 'favicon.ico'))
+
 
 @app.route('/')
 def handle_request(request):
@@ -19,10 +24,9 @@ async def feed(request, ws):
     clients.add(ws)
     while True:
         recv_message = await ws.recv()
-        id = recv_message
-        print(id)
+        sec_id = recv_message
         for client in clients:
-            await client.send(id)
+            await client.send(sec_id)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
