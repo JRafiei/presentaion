76a176be6defc98a1d4ef49a8e0a9b4c254bad1a
MJR <mohammad.rafiei69@gmail.com>
2019-04-18 22:49:25 +0430

    add items to the database and improve related template

--- apps/models.py
+++ apps/models.py
@@ -8,3 +8,11 @@ class BaseModel(Model):
 
     class Meta:
         database = db
+
+
+class Item(BaseModel):
+    title = CharField()
+    description = TextField()
+    filename = CharField()
+    created_date = DateTimeField(default=datetime.datetime.now)
+    
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sanic.response import json, html
 from sanic.websocket import WebSocketProtocol, ConnectionClosed
 from sanic_session import Session
 from sanic_jinja2 import SanicJinja2
-from apps.models import db
+from apps.models import db, Item
 
 app = Sanic()
 app.config.UPLOAD_PATH = os.path.join(os.path.dirname(__file__), 'uploads')
@@ -30,10 +30,15 @@ def handle_request(request):
 async def add_item(request):
     if request.method == 'POST':
         filename = request.files["file"][0].name
-        f = open(f'{app.config.UPLOAD_PATH}/{filename}', 'wb')
-        f.write(request.files["file"][0].body)
-        f.close()
+        with open(f'{app.config.UPLOAD_PATH}/{filename}', 'wb') as f: 
+            f.write(request.files["file"][0].body)
 
+        Item.create(
+            title=request.form.get('title'),
+            description=request.form.get('description'),
+            filename=filename
+        )
+        return {'message': "uploaded successfully!"}
     return {}
 
 
@@ -50,6 +55,7 @@ async def feed(request, ws):
             clients.remove(ws)
             break
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-H", "--host", default="localhost",
@@ -57,8 +63,8 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--port", default=8000,
                         help="server port")
     args = parser.parse_args()
-    # db.connect()
-    # db.create_tables([User, Tweet])
+    db.connect()
+    db.create_tables([Item])
     if not os.path.exists(app.config.UPLOAD_PATH):
         os.makedirs(app.config.UPLOAD_PATH)
     app.run(host=args.host, port=args.port)
